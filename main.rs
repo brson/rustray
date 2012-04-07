2e40580204bda35e71fea358140bb81ab2ef1856
Sebastian Sylvan <sebastian.sylvan@gmail.com>
2012-04-07 16:58:11 -0700

    Change default to something more sensible

--- main.rs
+++ main.rs
@@ -3,12 +3,12 @@ import io::writer;
 import raytracer::*;
 
 // TODO: These things should all be overridable by the command line
-const WIDTH : uint = 1024u;
-const HEIGHT : uint = 1024u;
+const WIDTH : uint = 256u;
+const HEIGHT : uint = 256u;
 const FOV : f32 = 3.14159f32 / 3f32 ;
-const SAMPLE_GRID_SIZE : uint = 4u;
-const NUM_GI_SAMPLES_SQRT: uint = 16u;
-const NUM_LIGHT_SAMPLES : uint = 64u;
+const SAMPLE_GRID_SIZE : uint = 1u;
+const NUM_GI_SAMPLES_SQRT: uint = 8u;
+const NUM_LIGHT_SAMPLES : uint = 32u;
 const MAX_TRACE_DEPTH : uint = 3u;
 const USE_SMOOTH_NORMALS_FOR_GI : bool = true;
 const USE_SMOOTH_NORMALS_FOR_DIRECT_LIGHTING : bool = true;
